7ba8a5e5ab9e3852664cf0fbac4fc9c0cae4ae96
Kaushal Chauhan <kaushalchauhan4199@gmail.com>
2024-03-06 09:28:03 +0530

    Project gemini-clone init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Sidebar from "./components/Sidebar/Sidebar"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return <div className="sidebar">Sidebar</div>;
+};
+
+export default Sidebar;
